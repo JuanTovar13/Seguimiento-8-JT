3bdc41f90c624b6bf2eb598a4046b907acdeba53
juantovar <juanstovarm@gmail.com>
2024-09-21 11:51:28 -0500

    ejercicio6

--- excercises/excercise6.js
+++ excercises/excercise6.js
@@ -1,15 +1,25 @@
 function sumarHastaNumero(num) {
     // TODO: Dado un número y sumar los números del 1 hasta ese número usando while.
-    let numero = 1;
+    let suma = 0;
     let i = 1;
+
     while(i <= num) {
-        console.log(i)
+        suma += i;
         i++;
-        const sumanumero = numero += i
+          
     }
-console.log(sumanumero)
+
+    return suma;
+    
 }
+let num = 100
+let resultado = sumarHastaNumero(num)
+
+
+
+
 console.log("-----------------------------------")
 console.log("EJERCICIO 6")
 console.log("-----------------------------------")
 sumarHastaNumero(100)
+console.log(resultado)
